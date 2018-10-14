d62d0eb468ddceeed15adcb142f55c267b490a2a
hillevieklow <hillevieklow@gmail.com>
2018-10-14 15:10:16 -0400

    creates users db and seeds db

--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,10 @@ ActiveRecord::Schema.define(version: 2018_10_14_014403) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "name"
+    t.string "password_digest"
+    t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+u1 = User.create(:name => "Hillevi", :email => "hillevi@domain.com", :password => "test")
+u2 = User.create(:name => "Jenny", :email => "jenny@domain.com", :password => "test", :admin => "true")
+u3 = User.create(:name => "Linnea", :email => "linnea@domain.com", :password => "test")
+
+a1 = Article.create(:title => "Types of Succulents", :content => "Lorem ipsum dolor sit amet, per dicunt regione scripserit ad, id ius quodsi appareat, unum erat legendos eos in. Duis animal ei vel. Vis debitis mandamus ut, dolorem fastidii at sit, unum vidit malis eu mei. Eam deserunt appellantur et, vis soluta placerat cu. Has ea corpora dissentiunt liberavisse, legere moderatius nec in, ne cum debitis adipiscing.")
+a2 = Article.create(:title => "Merry Christmas", :content => "Quaeque offendit ut qui. At posse vocent qui, vix numquam pericula petentium ad. Mea ad amet liberavisse. At vidit error percipitur pri, ius ei quod fugit concludaturque, ad duo detraxit efficiantur neglegentur. Ex pro quod animal conclusionemque, in mel facilisis intellegat temporibus, per ut nusquam indoctum. Usu ad affert virtute, deleniti posidonium ad mel.")
