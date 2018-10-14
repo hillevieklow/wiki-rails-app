be7267e4527b70b1753460cb8c6ff3ae12d4634f
hillevieklow <hillevieklow@gmail.com>
2018-10-14 15:09:43 -0400

    adds users resource and login / logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get '/login' => 'application#welcome'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+  
   resources :articles
+  resources :users
   root 'application#welcome', as: :welcome
 end
