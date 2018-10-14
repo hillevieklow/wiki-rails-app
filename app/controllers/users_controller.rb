c4c90b7d2ca198425074501443710634eac827b3
hillevieklow <hillevieklow@gmail.com>
2018-10-14 15:11:56 -0400

    init users controller commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+  before_action :require_logged_in, except: [:new, :create]
+
+  def index
+    @users = User.all.sort { |a,b| a.name <=> b.name }
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      session[:user_id] = @user.id
+      redirect_to user_path(@user.id)
+    else
+      render :new
+    end
+  end
+
+  def show
+    find_user
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
+
+  def find_user
+    @user = User.find(params[:id])
+  end
 end
