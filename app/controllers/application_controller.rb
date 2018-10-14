c8a169aae23318f40e5d3da85134f3208a3a6333
hillevieklow <hillevieklow@gmail.com>
2018-10-14 15:09:10 -0400

    adds app controller functionality and validations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::Base
+  helper_method :logged_in?, :is_admin?
+  before_action :current_user
+
+  def welcome
+    @user = User.new
+  end
+
+  def logged_in?
+    session[:user_id]
+  end
+
+  def is_admin?
+    current_user && @current_user.admin
+  end
+
+  private
+
+  def require_logged_in
+    redirect_to welcome_path unless logged_in?
+  end
+
+  def require_admin
+    redirect_to user_path(@current_user) if !is_admin?
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
