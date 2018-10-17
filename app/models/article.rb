8a5ec06797b0e972cf0e02d42e32ac8283202380
hillevieklow <hillevieklow@gmail.com>
2018-10-16 22:24:07 -0400

    class functions init

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,31 @@
 class Article < ApplicationRecord
-  belongs_to :topic, required: false
+  belongs_to :category
   belongs_to :user
   has_many :favorites
+
+  def self.sort_by(sort_column)
+    order = ["upvotes", "updated_at"].include?(sort_column) ? :desc : :asc
+    if sort_column == "category"
+      Article.joins(:category).merge(Category.order(name: order))
+    else
+      Article.order("#{sort_column} #{order}")
+    end
+  end
+
+  def category_name
+    self.category ? self.category.name : nil
+  end
+
+  def new_category=(data)
+    if !data.empty?
+        self.category = Category.find_or_create_by(:name => data)
+    end
+  end
+
+  def new_category
+  end
+
+  def self.get_latest(number_articles)
+    self.last(number_articles).sort { |x,y| y.created_at <=> x.created_at }
+  end
 end
