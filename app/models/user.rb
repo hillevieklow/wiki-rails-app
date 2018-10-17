c75cfe64dd72d83e264b7a3d23abf153d6be6814
hillevieklow <hillevieklow@gmail.com>
2018-10-16 22:24:56 -0400

    makes emails lowecase before saving into db

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   has_many :articles
   has_many :favorites, through: :articles
+
+  before_save { |user| user.email = user.email.downcase }
 end
