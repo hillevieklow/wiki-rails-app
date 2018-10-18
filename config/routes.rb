51d8c711c2b4125b6aeda516486083315b4044c9
hillevieklow <hillevieklow@gmail.com>
2018-10-17 20:24:31 -0400

    adds categories routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   get '/login' => 'application#welcome'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
-  
+
   resources :articles
   resources :users
+  resources :categories
   root 'application#welcome', as: :welcome
 end
